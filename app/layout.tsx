1cf85f86fb99efc8e69f5efad275305620df8935
Leonardo Aranguren S <leoaranguren10@gmail.com>
2025-06-06 14:01:05 -0400

    tryy another implementation of GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { GoogleTagManager } from '@next/third-parties/google';
 import { Amplify } from 'aws-amplify';
+// import Script from 'next/script';
 
 import outputs from '@/amplify_outputs.json';
 import { Footer } from '@/components/layout/footer/Footer';
@@ -48,14 +49,31 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            {/* <Script id="google-tag-manager" strategy="afterInteractive">
+                {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${EnviromentVariables.GTM_ID}');
+                `}
+            </Script> */}
+            <GoogleTagManager gtmId={EnviromentVariables.GTM_ID} />
             <body>
+                {/* <noscript>
+                    <iframe
+                        src={`https://www.googletagmanager.com/ns.html?id=${EnviromentVariables.GTM_ID}`}
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript> */}
                 <Providers>
                     <ThemeTransitionEnabler />
                     <NavBar />
                     <main>{children}</main>
                     <Footer />
                 </Providers>
-                <GoogleTagManager gtmId={EnviromentVariables.GTM_ID} />
             </body>
         </html>
     );
